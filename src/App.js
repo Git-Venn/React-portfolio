3e9043bc1af086ffde1f50756ae88b2f8eb9b9e5
Git-Venn <kylevennchang@gmail.com>
2024-10-25 23:02:17 +0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import About from './components/About';
@@ -7,15 +7,16 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}/>
           <Route path="about" element={<About />}/>
           <Route path="contact" element={<Contact />}/>
+          <Route path="*" element={<h1>404 Not Found</h1>}/>
         </Route>
       </Routes>
-    </>
+    </HashRouter>
   );
 }
 
